49c0b66b3939953bacaabc8502a6f2d7e3bb2dcd
Roberto <roberto@morales.com.co>
2018-09-06 20:31:31 -0500

    Opciones iniciales agregadas

--- app/src/main/java/com/holamundo/personasmovil/Registrar.java
+++ app/src/main/java/com/holamundo/personasmovil/Registrar.java
@@ -2,9 +2,10 @@ package com.holamundo.personasmovil;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class Registrar extends AppCompatActivity {
-
+   
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
